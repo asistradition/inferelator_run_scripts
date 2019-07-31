85b8ef20678e261843d2a3428190a7c7b2ac7d1e
CJackson <christopher@cajackson.org>
2019-07-31 10:40:35 -0400

    Revision stuff

--- jackson_2019/jackson_2019_figure5d_mtl_amusr.py
+++ jackson_2019/jackson_2019_figure5d_mtl_amusr.py
@@ -1,7 +1,5 @@
 from inferelator import utils
 from inferelator.postprocessing.results_processor_mtl import ResultsProcessorMultiTask
-from inferelator.regression.bbsr_multitask import BBSRByTaskRegressionWorkflow
-from inferelator.regression.elasticnet_multitask import ElasticNetByTaskRegressionWorkflow
 
 # Ugly hack for relative import from __main__ because fucking python, am I right?
 import os
@@ -26,17 +24,10 @@ if __name__ == '__main__':
     ws.start_mpcontrol_dask(100)
 
     utils.Debug.vprint("Generating Fig 5D", level=0)
-    # Figure 5D: STL
-    worker = ws.set_up_fig5a()
-    worker.append_to_path('output_dir', 'figure_5d_stl')
-    worker.priors_file = ws.YEASTRACT_PRIOR
-    worker.seeds = list(range(52, 62))
-    worker.run()
-    del worker
 
     # Figure 5D: MTL
     worker = ws.set_up_fig5a()
-    worker.append_to_path('output_dir', 'figure_5d_amusr_mtl')
+    worker.append_to_path('output_dir', 'figure_5d_mtl_amusr')
     worker.priors_file = ws.YEASTRACT_PRIOR
     worker.cv_workflow_type = "amusr"
     worker.cv_regression_type = "amusr"
@@ -45,15 +36,3 @@ if __name__ == '__main__':
     worker.task_expression_filter = "intersection"
     worker.run()
     del worker
-
-    # Figure 5D: MTL-BBSR
-    # worker = ws.set_up_fig5a()
-    # worker.append_to_path('output_dir', 'figure_5d_bbsr_mtl')
-    # worker.priors_file = ws.YEASTRACT_PRIOR
-    # worker.cv_workflow_type = "amusr"
-    # worker.cv_regression_type = BBSRByTaskRegressionWorkflow
-    # worker.cv_result_processor_type = ResultsProcessorMultiTask
-    # worker.seeds = list(range(52, 62))
-    # worker.task_expression_filter = "intersection"
-    # worker.run()
-    # del worker
