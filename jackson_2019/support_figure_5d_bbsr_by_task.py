6661a50f94544d432c7bec42015d321ce88afc4a
CJackson <christopher@cajackson.org>
2019-07-31 10:31:12 -0400

    Fix 5d

--- jackson_2019/support_figure_5d_bbsr_by_task.py
+++ jackson_2019/support_figure_5d_bbsr_by_task.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
 
     # Figure 5D: MTL
     worker = ws.set_up_fig5a()
-    worker.append_to_path('output_dir', 'figure_5d_multiple_stl')
-    worker.priors_file = "YEASTRACT_Both_20181118.tsv"
+    worker.append_to_path('output_dir', 'figure_5d_mtl_bbsr')
+    worker.priors_file = ws.YEASTRACT_PRIOR
     worker.cv_workflow_type = "amusr"
     worker.cv_regression_type = BBSRByTaskRegressionWorkflow
     worker.cv_result_processor_type = ResultsProcessorMultiTask
