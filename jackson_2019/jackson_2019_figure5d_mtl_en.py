d5556696538e65135c3a582f580e8fc3f494c4f8
asistradition <asistradition@users.noreply.github.com>
2019-10-30 15:16:09 -0400

    MTL task

--- jackson_2019/jackson_2019_figure5d_mtl_en.py
+++ jackson_2019/jackson_2019_figure5d_mtl_en.py
@@ -1,5 +1,6 @@
 from inferelator.postprocessing.results_processor_mtl import ResultsProcessorMultiTask
 from inferelator.regression.elasticnet_multitask import ElasticNetByTaskRegressionWorkflow
+from inferelator.preprocessing import single_cell
 
 # Ugly hack for relative import from __main__ because fucking python, am I right?
 import os
@@ -19,6 +20,22 @@ except ImportError:
 
     filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "jackson_2019_workflow_setup.py")
     ws = importlib.machinery.SourceFileLoader("ws", filename).load_module()
+
+
+def set_up_mtl_workflow(wkf):
+    # Jackson single cell task
+    task = wkf.create_task(task_name="Jackson_2019",
+                           expression_matrix_file="103118_SS_Data.tsv.gz",
+                           expression_matrix_columns_are_genes=True,
+                           extract_metadata_from_expression_matrix=True,
+                           expression_matrix_metadata=['Genotype', 'Genotype_Group', 'Replicate', 'Condition',
+                                                       'tenXBarcode'],
+                           workflow_type="single-cell",
+                           count_minimum=0.05,
+                           tasks_from_metadata=True,
+                           meta_data_task_column="Condition")
+    task.add_preprocess_step(single_cell.log2_data)
+
 
 if __name__ == '__main__':
     ws.start_mpcontrol_dask(60)
