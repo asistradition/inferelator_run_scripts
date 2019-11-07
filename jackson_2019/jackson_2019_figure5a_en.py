92c3f66c3fb62071a690ce4e78827147fb967989
asistradition <asistradition@users.noreply.github.com>
2019-11-07 16:34:15 -0500

    diff names

--- jackson_2019/jackson_2019_figure5a_en.py
+++ jackson_2019/jackson_2019_figure5a_en.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     # Figure 5A: Shuffled Priors
     worker = ws.set_up_fig5a(regression="elasticnet")
     worker.workflow.set_regression_parameters(l1_ratio=1, max_iter=2000, min_coef=0)
-    worker.append_to_path('output_dir', 'figure_5a_shuffled')
+    worker.append_to_path('output_dir', 'en_figure_5a_shuffled')
     worker.workflow.set_shuffle_parameters(shuffle_prior_axis=0)
     worker.run()
     del worker
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     # Figure 5A: Random Data
     worker = ws.set_up_fig5a(regression="elasticnet")
     worker.workflow.set_regression_parameters(l1_ratio=1, max_iter=2000, min_coef=0)
-    worker.append_to_path('output_dir', 'figure_5a_neg_data')
+    worker.append_to_path('output_dir', 'en_figure_5a_neg_data')
     worker.workflow.wkf.set_file_paths(expression_matrix_file='110518_SS_NEG_Data.tsv.gz')
     worker.run()
     del worker
@@ -43,14 +43,14 @@ if __name__ == '__main__':
     # Figure 5A: No Imputation
     worker = ws.set_up_fig5a(regression="elasticnet")
     worker.workflow.set_regression_parameters(l1_ratio=1, max_iter=2000, min_coef=0)
-    worker.append_to_path('output_dir', 'figure_5a_no_impute')
+    worker.append_to_path('output_dir', 'en_figure_5a_no_impute')
     worker.run()
     del worker
 
     # Figure 5A: MAGIC
     worker = ws.set_up_fig5a(regression="elasticnet")
     worker.workflow.set_regression_parameters(l1_ratio=1, max_iter=2000, min_coef=0)
-    worker.append_to_path('output_dir', 'figure_5a_magic')
+    worker.append_to_path('output_dir', 'en_figure_5a_magic')
     worker.workflow.wkf.set_file_paths(expression_matrix_file='MAGIC_DATA.tsv.gz')
     worker.workflow.preprocessing_workflow = list()
     worker.workflow.count_minimum = None
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     # Figure 5A: scImpute
     worker = ws.set_up_fig5a()
     worker.workflow.set_regression_parameters(l1_ratio=1, max_iter=2000, min_coef=0)
-    worker.append_to_path('output_dir', 'figure_5a_scImpute')
+    worker.append_to_path('output_dir', 'en_figure_5a_scImpute')
     worker.workflow.wkf.set_file_paths(expression_matrix_file='SCIMPUTE_DATA.tsv.gz')
     worker.run()
     del worker
