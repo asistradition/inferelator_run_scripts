01b13e303949a7d83dcb5132d0c1ef92e2177f62
asistradition <asistradition@users.noreply.github.com>
2020-06-11 11:41:59 -0400

    Velo package

--- inferelator_0_5_0/dewakks_velocity_onlyv.py
+++ inferelator_0_5_0/dewakks_velocity_onlyv.py
@@ -4,6 +4,7 @@ from inferelator.distributed.inferelator_mp import MPControl
 from inferelator.preprocessing import single_cell
 from inferelator.workflow import inferelator_workflow
 from inferelator.velocity_workflow import VelocityWorkflow
+import os
 
 utils.Debug.set_verbose_level(1)
 
@@ -20,7 +21,7 @@ TF_NAMES = "tf_names_gold_standard.txt"
 YEASTRACT_TF_NAMES = "tf_names_yeastract.txt"
 
 INPUT_DIR = '/mnt/ceph/users/cjackson/inferelator/data/yeast'
-OUTPUT_PATH = '/mnt/ceph/users/cjackson/jackson_2019_inferelator_v050/tau43'
+OUTPUT_PATH = '/mnt/ceph/users/cjackson/jackson_2019_inferelator_v050/'
 
 if __name__ == '__main__':
     MPControl.set_multiprocess_engine("dask-cluster")
@@ -29,9 +30,9 @@ if __name__ == '__main__':
     MPControl.client.add_worker_conda("source ~/.local/anaconda3/bin/activate inferelator")
     MPControl.connect()
 
-wkf = inferelator_workflow("bbsr", VelocityWorkflow)
+wkf = inferelator_workflow("stars", VelocityWorkflow)
 wkf.set_file_paths(input_dir=INPUT_DIR,
-                   output_dir=OUTPUT_PATH,
+                   output_dir=os.path.join(OUTPUT_PATH, "only_v"),
                    gold_standard_file='gold_standard.tsv',
                    priors_file=YEASTRACT_PRIOR,
                    tf_names_file=YEASTRACT_TF_NAMES)
@@ -42,7 +43,29 @@ wkf.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
 wkf.set_run_parameters(num_bootstraps=5)
 wkf.set_count_minimum(0.05)
 wkf.add_preprocess_step(single_cell.log2_data)
-wkf.tau = 43.28
+wkf._use_precalculated_decay_constants = False
+
+cv_wrap = crossvalidation_workflow.CrossValidationManager(wkf)
+cv_wrap.add_gridsearch_parameter('random_seed', list(range(42, 52)))
+
+cv_wrap.run()
+del cv_wrap
+
+wkf = inferelator_workflow("stars", VelocityWorkflow)
+wkf.set_file_paths(input_dir=INPUT_DIR,
+                   output_dir=os.path.join(OUTPUT_PATH, "only_v_shuffle"),
+                   gold_standard_file='gold_standard.tsv',
+                   priors_file=YEASTRACT_PRIOR,
+                   tf_names_file=YEASTRACT_TF_NAMES)
+wkf.set_expression_file(h5ad=DATA_FILE, h5_layer="smooth_count")
+wkf.set_velocity_parameters(velocity_file_name=DATA_FILE, velocity_file_type="h5ad", velocity_file_layer="pv")
+wkf.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
+                                   cv_split_ratio=0.5)
+wkf.set_run_parameters(num_bootstraps=5)
+wkf.set_count_minimum(0.05)
+wkf.add_preprocess_step(single_cell.log2_data)
+wkf._use_precalculated_decay_constants = False
+wkf.set_shuffle_parameters(shuffle_prior_axis=0)
 
 cv_wrap = crossvalidation_workflow.CrossValidationManager(wkf)
 cv_wrap.add_gridsearch_parameter('random_seed', list(range(42, 52)))
