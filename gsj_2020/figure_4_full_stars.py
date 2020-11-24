0229579b7c44cbeb10111621fec49e3838e6cc43
asistradition <asistradition@users.noreply.github.com>
2020-11-24 13:35:35 -0500

    Fix full paths

--- gsj_2020/figure_4_full_stars.py
+++ gsj_2020/figure_4_full_stars.py
@@ -13,7 +13,7 @@ TF_NAMES = "tf_names_gold_standard.txt"
 YEASTRACT_TF_NAMES = "tf_names_yeastract.txt"
 
 INPUT_DIR = '/mnt/ceph/users/cjackson/inferelator/data/yeast'
-OUTPUT_PATH = '/mnt/ceph/users/cjackson/gsj_2020_bbsr_fig4_stars_full'
+OUTPUT_PATH = '/mnt/ceph/users/cjackson/gsj_2020_stars_fig4_full'
 
 utils.Debug.set_verbose_level(1)
 
