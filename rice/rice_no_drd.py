4206a82db690fa3494ee17bd48fb0b51b209dced
asistradition <asistradition@users.noreply.github.com>
2021-07-14 10:51:15 -0400

    Add expression data

--- rice/rice_no_drd.py
+++ rice/rice_no_drd.py
@@ -6,8 +6,8 @@ from inferelator.distributed.inferelator_mp import MPControl
 
 
 CONDA_ACTIVATE_PATH = '~/.local/anaconda3/bin/activate'
-EXPRESSION_MATRIX_METADATA = ['Genotype', 'Genotype_Group', 'Replicate', 'Condition', 'tenXBarcode']
 
+EXPRESSION_FILE = "Oryza_expression_data_MZ82_2021.h5ad"
 MOTIF_PRIOR = "EGRIN_PRIOR_MOTIF.tsv"
 NETWORK_PRIOR = "EGRIN_PRIOR_INFERRED.tsv"
 
@@ -17,10 +17,12 @@ OUTPUT_PATH = '/mnt/ceph/users/cjackson/mz82_rice'
 utils.Debug.set_verbose_level(1)
 
 
-def set_up_workflow(wkf):
+def set_up_workflow(wkf, prior_file = MOTIF_PRIOR):
     wkf.set_file_paths(input_dir=INPUT_DIR,
-                       output_dir=OUTPUT_PATH,
-                       gold_standard_file='gold_standard.tsv')
+                       output_dir=OUTPUT_PATH)
+
+    worker.set_file_paths(priors_file=prior_file, gold_standard_file=prior_file)
+    wkf.set_expression_file(h5ad=EXPRESSION_FILE)
 
 
     wkf.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
@@ -51,9 +53,7 @@ if __name__ == '__main__':
     worker = workflow.inferelator_workflow(regression="stars", workflow="tfa")
     worker.drd_driver = None
 
-    set_up_workflow(worker)
-    worker.set_file_paths(priors_file=NETWORK_PRIOR)
-
+    set_up_workflow(worker, prior_file=NETWORK_PRIOR)
     worker.append_to_path('output_dir', 'network_prior_stars')
     cv_wrap = set_up_cv_seeds(worker)
     cv_wrap.run()
@@ -66,8 +66,7 @@ if __name__ == '__main__':
     worker = workflow.inferelator_workflow(regression="stars", workflow="tfa")
     worker.drd_driver = None
 
-    set_up_workflow(worker)
-    worker.set_file_paths(priors_file=MOTIF_PRIOR)
+    set_up_workflow(worker, prior_file=MOTIF_PRIOR)
 
     worker.append_to_path('output_dir', 'motif_prior_stars')
     cv_wrap = set_up_cv_seeds(worker)
@@ -81,8 +80,7 @@ if __name__ == '__main__':
     worker = workflow.inferelator_workflow(regression="bbsr", workflow="tfa")
     worker.drd_driver = None
 
-    set_up_workflow(worker)
-    worker.set_file_paths(priors_file=NETWORK_PRIOR)
+    set_up_workflow(worker, prior_file=NETWORK_PRIOR)
 
     worker.append_to_path('output_dir', 'network_prior_bbsr')
     cv_wrap = set_up_cv_seeds(worker)
@@ -96,8 +94,7 @@ if __name__ == '__main__':
     worker = workflow.inferelator_workflow(regression="bbsr", workflow="tfa")
     worker.drd_driver = None
 
-    set_up_workflow(worker)
-    worker.set_file_paths(priors_file=MOTIF_PRIOR)
+    set_up_workflow(worker, prior_file=MOTIF_PRIOR)
 
     worker.append_to_path('output_dir', 'motif_prior_bbsr')
     cv_wrap = set_up_cv_seeds(worker)
