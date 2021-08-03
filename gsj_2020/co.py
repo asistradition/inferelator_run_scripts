5c8bd11e2e1093ba579109b90c9e8c6608e51ea1
asistradition <asistradition@users.noreply.github.com>
2021-08-03 19:07:43 -0400

    co benching

--- gsj_2020/co.py
+++ gsj_2020/co.py
@@ -1,24 +1,19 @@
 from inferelator import utils
 from inferelator import workflow
 from inferelator import crossvalidation_workflow
-from inferelator.regression.base_regression import BaseRegression
-from inferelator.benchmarking.celloracle import CellOracleWorkflow
+from inferelator.benchmarking.celloracle import CellOracleWorkflow ,CellOracleRegression
 from inferelator.distributed.inferelator_mp import MPControl
 
-
-CONDA_ACTIVATE_PATH = '~/.local/anaconda3/bin/activate'
-EXPRESSION_MATRIX_METADATA = ['Genotype', 'Genotype_Group', 'Replicate', 'Condition', 'tenXBarcode']
-
 YEASTRACT_PRIOR = "YEASTRACT_20190713_BOTH.tsv"
-
-TF_NAMES = "tf_names_gold_standard.txt"
 YEASTRACT_TF_NAMES = "tf_names_yeastract.txt"
 
-INPUT_DIR = '/mnt/ceph/users/cjackson/inferelator/data/yeast'
-OUTPUT_PATH = '/mnt/ceph/users/cjackson/gsj_2020_celloracle'
+INPUT_DIR = '/home/cj59/Documents/celloracle'
+OUTPUT_PATH = '/scratch/cj59/gsj_2020_celloracle'
 
 utils.Debug.set_verbose_level(1)
 
+MPControl.set_multiprocess_engine("local")
+MPControl.connect()
 
 def set_up_workflow(wkf):
     wkf.set_file_paths(input_dir=INPUT_DIR,
@@ -40,7 +35,7 @@ def set_up_cv_seeds(wkf):
 
 if __name__ == '__main__':
 
-    worker = workflow.inferelator_workflow(regression=BaseRegression, workflow=CellOracleWorkflow)
+    worker = workflow.inferelator_workflow(regression=CellOracleRegression, workflow=CellOracleWorkflow)
     set_up_workflow(worker)
     worker.append_to_path('output_dir', 'experimental')
 
@@ -50,7 +45,7 @@ if __name__ == '__main__':
     del cv_wrap
     del worker
 
-    worker = workflow.inferelator_workflow(regression=BaseRegression, workflow=CellOracleWorkflow)
+    worker = workflow.inferelator_workflow(regression=CellOracleRegression, workflow=CellOracleWorkflow)
     set_up_workflow(worker)
     worker.append_to_path('output_dir', 'shuffle')
     worker.set_shuffle_parameters(shuffle_prior_axis=0)
@@ -61,7 +56,7 @@ if __name__ == '__main__':
     del cv_wrap
     del worker
 
-    worker = workflow.inferelator_workflow(regression=BaseRegression, workflow=CellOracleWorkflow)
+    worker = workflow.inferelator_workflow(regression=CellOracleRegression, workflow=CellOracleWorkflow)
     set_up_workflow(worker)
     worker.append_to_path('output_dir', 'noise')
     worker.set_shuffle_parameters(make_data_noise=True)
