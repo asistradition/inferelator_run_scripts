af662062457e75df513b62c26db7c4d441ea3a4e
asistradition <asistradition@users.noreply.github.com>
2021-08-16 10:41:53 -0400

    Finish celloracle

--- gsj_2020/co_experimental_priornoise.py
+++ gsj_2020/co_experimental_priornoise.py
@@ -28,7 +28,7 @@ def set_up_workflow(wkf):
 
 def set_up_cv_seeds(wkf):
     cv = crossvalidation_workflow.CrossValidationManager(wkf)
-    cv.add_gridsearch_parameter('random_seed', list(range(42, 52)))
+    cv.add_gridsearch_parameter('random_seed', list(range(51, 52)))
     return cv
 
 
