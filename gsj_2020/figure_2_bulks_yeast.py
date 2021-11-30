539b15079a190dcc50c62c90a9be7f71a1990763
asistradition <asistradition@users.noreply.github.com>
2021-11-30 15:17:46 -0500

    Check raw microarray

--- gsj_2020/figure_2_bulks_yeast.py
+++ gsj_2020/figure_2_bulks_yeast.py
@@ -1,5 +1,5 @@
 # Load modules
-from inferelator import inferelator_workflow, inferelator_verbose_level, MPControl
+from inferelator import inferelator_workflow, inferelator_verbose_level, MPControl, crossvalidation_workflow
 from inferelator.benchmarking.scenic import SCENICWorkflow, SCENICRegression
 from inferelator.distributed.inferelator_mp import MPControl
 
@@ -11,7 +11,7 @@ inferelator_verbose_level(1)
 DATA_DIR = '~/repos/inferelator/data/yeast'
 OUTPUT_DIR = '/scratch/cj59/yeast_inference'
 
-PRIORS_FILE_NAME = 'gold_standard.tsv.gz'
+PRIORS_FILE_NAME = 'YEASTRACT_20190713_BOTH.tsv'
 GOLD_STANDARD_FILE_NAME = 'gold_standard.tsv.gz'
 TF_LIST_FILE_NAME = 'tf_names.tsv'
 
@@ -30,61 +30,207 @@ def set_up_workflow(wkf):
                        tf_names_file=TF_LIST_FILE_NAME,
                        priors_file=PRIORS_FILE_NAME,
                        gold_standard_file=GOLD_STANDARD_FILE_NAME)
-    wkf._do_preprocessing = False
-    wkf.do_scenic = False
+    
     wkf.set_output_file_names(curve_data_file_name="metric_curve.tsv.gz")
     return wkf
 
+def set_up_cv_seeds(wkf):
+    cv = crossvalidation_workflow.CrossValidationManager(wkf)
+    cv.add_gridsearch_parameter('random_seed', list(range(42, 52)))
+    return cv
 
 # Data Set 1
 
-# Create a worker
-worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
-worker = set_up_workflow(worker)
-worker.set_expression_file(tsv="calico_expression_matrix_log2.tsv.gz")
-worker.set_file_properties(extract_metadata_from_expression_matrix=True,
-                           expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism', 'time'],
-                           metadata_handler="nonbranching")
-worker.adjacency_method = "grnboost2"
+if __name__ == '__main__':
 
-worker.append_to_path("output_dir", "set1_grnboost")
-worker.run()
+    # Create a worker
+    worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
+    worker = set_up_workflow(worker)
+    worker.set_expression_file(tsv="calico_expression_matrix_raw_microarray.tsv.gz")
+    worker.set_file_properties(extract_metadata_from_expression_matrix=True,
+                            expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism', 'time'],
+                            metadata_handler="nonbranching")
+    worker.adjacency_method = "grnboost2"
+    worker.set_output_file_names(curve_data_file_name="metric_curve.tsv.gz")
+    worker._do_preprocessing = False
+    worker.do_scenic = False
+    worker.append_to_path("output_dir", "set1_raw_grnboost")
+    worker.run()
 
-# Create a worker
-worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
-worker = set_up_workflow(worker)
-worker.set_expression_file(tsv="calico_expression_matrix_log2.tsv.gz")
-worker.set_file_properties(extract_metadata_from_expression_matrix=True,
-                           expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism', 'time'],
-                           metadata_handler="nonbranching")
-worker.adjacency_method = "genie3"
+    # BBSR
+    worker = inferelator_workflow(regression="bbsr", workflow="tfa")
+    worker = set_up_workflow(worker)
+    worker.set_expression_file(tsv="calico_expression_matrix_raw_microarray.tsv.gz")
+    worker.set_file_properties(extract_metadata_from_expression_matrix=True,
+                            expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism', 'time'],
+                            metadata_handler="nonbranching")
+    worker.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
+                                        cv_split_ratio=0.2)
+    worker.set_run_parameters(num_bootstraps=5)
+    worker.append_to_path("output_dir", "set1_raw_bbsr")
+    worker.set_output_file_names(curve_data_file_name="metric_curve.tsv.gz")
 
-worker.append_to_path("output_dir", "set1_genie3")
-worker.run()
+    cv_wrap = set_up_cv_seeds(worker)
+    cv_wrap.run()
 
-# Data Set 2
+    del cv_wrap
+    del worker
 
-# Create a worker
-worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
-worker = set_up_workflow(worker)
-worker.set_expression_file(tsv="kostya_microarray_yeast.tsv.gz")
-worker.set_file_properties(extract_metadata_from_expression_matrix=True,
-                           expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
-                           metadata_handler="branching")
-worker.adjacency_method = "grnboost2"
+    # STARS-LASSO
+    worker = inferelator_workflow(regression="stars", workflow="tfa")
+    worker = set_up_workflow(worker)
+    worker.set_expression_file(tsv="calico_expression_matrix_raw_microarray.tsv.gz")
+    worker.set_file_properties(extract_metadata_from_expression_matrix=True,
+                            expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism', 'time'],
+                            metadata_handler="nonbranching")
+    worker.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
+                                        cv_split_ratio=0.2)
+    worker.set_run_parameters(num_bootstraps=5)
+    worker.append_to_path("output_dir", "set1_raw_stars")
+    worker.set_output_file_names(curve_data_file_name="metric_curve.tsv.gz")
 
-worker.append_to_path("output_dir", "set2_grnboost")
-worker.run()
+    cv_wrap = set_up_cv_seeds(worker)
+    cv_wrap.run()
 
-# Create a worker
-worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
-worker = set_up_workflow(worker)
-worker.set_expression_file(tsv="kostya_microarray_yeast.tsv.gz")
-worker.set_file_properties(extract_metadata_from_expression_matrix=True,
-                           expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
-                           metadata_handler="branching")
-worker.adjacency_method = "genie3"
+    del cv_wrap
+    del worker
 
-worker.append_to_path("output_dir", "set2_genie3")
-worker.run()
+    # BBSR-BY-TASK
+    worker = inferelator_workflow(regression="bbsr", workflow="multitask")
+    worker = set_up_workflow(worker)
 
+    # Calico data task
+    task1 = worker.create_task(task_name="Calico_2019",
+                                expression_matrix_file="calico_expression_matrix_raw.tsv.gz",
+                                expression_matrix_columns_are_genes=True,
+                                extract_metadata_from_expression_matrix=True,
+                                expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism',
+                                                            'time'],
+                                workflow_type="tfa",
+                                metadata_handler="nonbranching")
+
+    # Kostya data task
+    task2 = worker.create_task(task_name="Kostya_2019",
+                                expression_matrix_file="kostya_microarray_yeast.tsv.gz",
+                                expression_matrix_columns_are_genes=True,
+                                extract_metadata_from_expression_matrix=True,
+                                expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
+                                workflow_type="tfa",
+                                metadata_handler="branching")
+
+    worker.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
+                                        cv_split_ratio=0.2)
+    worker.set_run_parameters(num_bootstraps=5)
+
+    worker.append_to_path("output_dir", "set1_raw_joint_bbsr")
+    cv_wrap = set_up_cv_seeds(worker)
+    cv_wrap.run()
+
+    del cv_wrap
+    del worker
+
+    # STARS-BY-TASK
+    worker = inferelator_workflow(regression="stars", workflow="multitask")
+    worker = set_up_workflow(worker)
+
+    # Calico data task
+    task1 = worker.create_task(task_name="Calico_2019",
+                                expression_matrix_file="calico_expression_matrix_raw.tsv.gz",
+                                expression_matrix_columns_are_genes=True,
+                                extract_metadata_from_expression_matrix=True,
+                                expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism',
+                                                            'time'],
+                                workflow_type="tfa",
+                                metadata_handler="nonbranching")
+
+    # Kostya data task
+    task2 = worker.create_task(task_name="Kostya_2019",
+                                expression_matrix_file="kostya_microarray_yeast.tsv.gz",
+                                expression_matrix_columns_are_genes=True,
+                                extract_metadata_from_expression_matrix=True,
+                                expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
+                                workflow_type="tfa",
+                                metadata_handler="branching")
+    
+    worker.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
+                                        cv_split_ratio=0.2)
+    worker.set_run_parameters(num_bootstraps=5)
+
+    worker.append_to_path("output_dir", "set1_raw_joint_stars")
+    cv_wrap = set_up_cv_seeds(worker)
+    cv_wrap.run()
+
+    del cv_wrap
+    del worker
+
+    # AMUSR
+    worker = inferelator_workflow(regression="amusr", workflow="multitask")
+    worker = set_up_workflow(worker)
+
+    # Calico data task
+    task1 = worker.create_task(task_name="Calico_2019",
+                                expression_matrix_file="calico_expression_matrix_raw.tsv.gz",
+                                extract_metadata_from_expression_matrix=True,
+                                expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism',
+                                                            'time'],
+                                workflow_type="tfa",
+                                metadata_handler="nonbranching")
+
+    # Kostya data task
+    task2 = worker.create_task(task_name="Kostya_2019",
+                                expression_matrix_file="kostya_microarray_yeast.tsv.gz",
+                                extract_metadata_from_expression_matrix=True,
+                                expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
+                                workflow_type="tfa",
+                                metadata_handler="branching")
+
+    worker.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
+                                        cv_split_ratio=0.2)
+    worker.set_run_parameters(num_bootstraps=5, use_numba=True)
+
+    worker.append_to_path("output_dir", "set1_raw_joint_amusr")
+    cv_wrap = set_up_cv_seeds(worker)
+    cv_wrap.run()
+
+    del cv_wrap
+    del worker
+
+    """
+    # Create a worker
+    worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
+    worker = set_up_workflow(worker)
+    worker.set_expression_file(tsv="calico_expression_matrix_raw_microarray.tsv.gz")
+    worker.set_file_properties(extract_metadata_from_expression_matrix=True,
+                            expression_matrix_metadata=['TF', 'strain', 'date', 'restriction', 'mechanism', 'time'],
+                            metadata_handler="nonbranching")
+    worker.adjacency_method = "genie3"
+
+    worker.append_to_path("output_dir", "set1_genie3")
+    worker.run()
+
+    # Data Set 2
+
+    # Create a worker
+    worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
+    worker = set_up_workflow(worker)
+    worker.set_expression_file(tsv="kostya_microarray_yeast.tsv.gz")
+    worker.set_file_properties(extract_metadata_from_expression_matrix=True,
+                            expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
+                            metadata_handler="branching")
+    worker.adjacency_method = "grnboost2"
+
+    worker.append_to_path("output_dir", "set2_grnboost")
+    worker.run()
+
+    # Create a worker
+    worker = inferelator_workflow(regression=SCENICRegression, workflow=SCENICWorkflow)
+    worker = set_up_workflow(worker)
+    worker.set_expression_file(tsv="kostya_microarray_yeast.tsv.gz")
+    worker.set_file_properties(extract_metadata_from_expression_matrix=True,
+                            expression_matrix_metadata=['isTs', 'is1stLast', 'prevCol', 'del.t', 'condName'],
+                            metadata_handler="branching")
+    worker.adjacency_method = "genie3"
+
+    worker.append_to_path("output_dir", "set2_genie3")
+    worker.run()
+    """
