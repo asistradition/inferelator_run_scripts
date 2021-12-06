89bf42910b7a7b5b1d34d093f7f3665b6fff6056
asistradition <asistradition@users.noreply.github.com>
2021-12-06 18:25:01 -0500

    amusr gs

--- inferelator_0_5_0/amusr_yeast_gs.py
+++ inferelator_0_5_0/amusr_yeast_gs.py
@@ -37,7 +37,7 @@ def set_up_workflow(wkf):
 
     wkf.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True,
                                        cv_split_ratio=0.2)
-    wkf.set_run_parameters(num_bootstraps=5)
+    wkf.set_run_parameters(num_bootstraps=5, use_numba=True)
     wkf.set_count_minimum(0.05)
 
 
