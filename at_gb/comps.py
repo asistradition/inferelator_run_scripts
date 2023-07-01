eae70342622d477aa6e41080c17f575dc2cb2f6f
asistradition <asistradition@users.noreply.github.com>
2023-07-01 15:39:48 -0400

    Fix c&p

--- at_gb/comps.py
+++ at_gb/comps.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                 tf_names_file=AT_TF,
                 priors_file=YEAST_PRIOR,
                 gold_standard_file=GOLD_STANDARD_FILE_NAME,
-                gengene_metadata_file=AT_GENES
+                gene_metadata_file=AT_GENES
             )
 
         elif yeast:
